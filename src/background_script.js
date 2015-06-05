29d1f7e50bab47b6caa485df4e35e3883dc24091
camigreenall <cami.greenall@byu.net>
2015-06-05 09:51:01 -0600

    Formatted js files

--- src/background_script.js
+++ src/background_script.js
@@ -8,146 +8,147 @@ var newTabId = -1;
 var moverInterval;
 
 var tabAutostart = false;
-if (localStorage["autostart"]) { 
-	if (localStorage["autostart"] == 'true') {
-		tabAutostart = true;
-	} else {
-		tabAutostart = false;
-	}
+if (localStorage["autostart"]) {
+  if (localStorage["autostart"] == 'true') {
+    tabAutostart = true;
+  } else {
+    tabAutostart = false;
+  }
 }
 
 var urls = [];
-if(localStorage["urls"]) {
-	urls = JSON.parse(localStorage["urls"]);
+if (localStorage["urls"]) {
+  urls = JSON.parse(localStorage["urls"]);
 }
 var urlsIntervals = [];
-if(localStorage["urlsIntervals"]) {
-	urlsIntervals = JSON.parse(localStorage["urlsIntervals"]);
+if (localStorage["urlsIntervals"]) {
+  urlsIntervals = JSON.parse(localStorage["urlsIntervals"]);
 }
 
 var urlsIndex = 0;
 var currTabId = -1;
 var nextTabId = -1;
 
-function include(arr,obj) {
-    return (arr.indexOf(obj) != -1);
+function include(arr, obj) {
+  return (arr.indexOf(obj) != -1);
 }
 
 function activeInWindow(windowId) {
-	for(i in activeWindows) {
-		if(activeWindows[i] == windowId) {
-			return true;
-		}
-	}
+  for (i in activeWindows) {
+    if (activeWindows[i] == windowId) {
+      return true;
+    }
+  }
 }
 
 // Setup Initial Badge Text
-var badgeColor = [139,137,137,137];
+var badgeColor = [139, 137, 137, 137];
 chrome.browserAction.setBadgeBackgroundColor({color: badgeColor});
 
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-	var windowId = tab.windowId;
-	currWindowId = tab.windowId;
-	if (activeInWindow(windowId)) {
-		stop(windowId);
-	} else {
-		go(windowId);
-	}
-});	
+chrome.browserAction.onClicked.addListener(function (tab) {
+  var windowId = tab.windowId;
+  currWindowId = tab.windowId;
+  if (activeInWindow(windowId)) {
+    stop(windowId);
+  } else {
+    go(windowId);
+  }
+});
 
 function badgeTabs(windowId, text) {
-	chrome.tabs.getAllInWindow(windowId, function(tabs) {
-		for(i in tabs) {
-			switch (text)
-			{
-			case 'on':
-			  chrome.browserAction.setBadgeText({text:"\u2022"});
-			  chrome.browserAction.setBadgeBackgroundColor({color:[0,255,0,100]});
-			  break;
-			case '':
-			  chrome.browserAction.setBadgeText({text:"\u00D7"});
-			  chrome.browserAction.setBadgeBackgroundColor({color:[255,0,0,100]});
-			  break;
-			default:
-			  chrome.browserAction.setBadgeText({text:""});
-			}
-		}	
-	});
+  chrome.tabs.getAllInWindow(windowId, function (tabs) {
+    for (i in tabs) {
+      switch (text) {
+        case 'on':
+          chrome.browserAction.setBadgeText({text: "\u2022"});
+          chrome.browserAction.setBadgeBackgroundColor({color: [0, 255, 0, 100]});
+          break;
+        case '':
+          chrome.browserAction.setBadgeText({text: "\u00D7"});
+          chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 100]});
+          break;
+        default:
+          chrome.browserAction.setBadgeText({text: ""});
+      }
+    }
+  });
 }
 
 // Start on a specific window
 function go(windowId) {
-	if(urls.length > 0) {
-		chrome.tabs.query({
-			'active': false,
-			'windowId': currWindowId
-		}, function(currTabs) {
-			for(i = 0; i < currTabs.length; i++) {
-				chrome.tabs.remove(currTabs[i].id);
-			}
-		});
-		chrome.tabs.onUpdated.addListener(startTimer);
-		activeWindows.push(windowId);
-		badgeTabs(windowId, 'on');
-		moveTab();
-	}
+  if (urls.length > 0) {
+    chrome.tabs.query({
+      'active': false,
+      'windowId': currWindowId
+    }, function (currTabs) {
+      for (i = 0; i < currTabs.length; i++) {
+        chrome.tabs.remove(currTabs[i].id);
+      }
+    });
+    chrome.tabs.onUpdated.addListener(startTimer);
+    activeWindows.push(windowId);
+    badgeTabs(windowId, 'on');
+    moveTab();
+  }
 }
 
 function startTimer(tabId, changeInfo, tab) {
-	clearInterval(moverInterval);
-	if(tab.status == 'complete') {
-		newTabId = tab;
-		var intervalIndex = urlsIndex - 2;
-		if(intervalIndex < 0) {
-			intervalIndex = urls.length + intervalIndex;
-		}
-		var delay = urlsIntervals[intervalIndex] * 1000;
-		moverInterval = setInterval(function() { moveTab2() }, delay);
-	}
+  clearInterval(moverInterval);
+  if (tab.status == 'complete') {
+    newTabId = tab;
+    var intervalIndex = urlsIndex - 2;
+    if (intervalIndex < 0) {
+      intervalIndex = urls.length + intervalIndex;
+    }
+    var delay = urlsIntervals[intervalIndex] * 1000;
+    moverInterval = setInterval(function () {
+      moveTab2()
+    }, delay);
+  }
 }
 
 // Stop on a specific window
 function stop(windowId) {
-	clearInterval(moverInterval);
-    console.log('Stopped.');
-    chrome.tabs.onUpdated.removeListener(startTimer);
-	var index = activeWindows.indexOf(windowId);
-	if(index >= 0) {
-		activeWindows.splice(index);
-		badgeTabs(windowId, '');
-	}
+  clearInterval(moverInterval);
+  console.log('Stopped.');
+  chrome.tabs.onUpdated.removeListener(startTimer);
+  var index = activeWindows.indexOf(windowId);
+  if (index >= 0) {
+    activeWindows.splice(index);
+    badgeTabs(windowId, '');
+  }
 }
 
 // Switches to next URL in list, loops.
 function moveTab() {
-	badgeTabs(currWindowId, 'on');
-	chrome.tabs.create({
-		url: urls[urlsIndex], 
-		selected: false
-	});
-	urlsIndex++;
-	if(urlsIndex == urls.length) {
-		urlsIndex = 0;
-	}
-	clearInterval(moverInterval);
+  badgeTabs(currWindowId, 'on');
+  chrome.tabs.create({
+    url: urls[urlsIndex],
+    selected: false
+  });
+  urlsIndex++;
+  if (urlsIndex == urls.length) {
+    urlsIndex = 0;
+  }
+  clearInterval(moverInterval);
 }
 
 // Deletes the current tab.
 function moveTab2() {
-	chrome.tabs.query( {
-		windowId: currWindowId
-	}, function(tabs2) {
-		chrome.tabs.remove(tabs2[0].id);
-		moveTab();
-	});
+  chrome.tabs.query({
+    windowId: currWindowId
+  }, function (tabs2) {
+    chrome.tabs.remove(tabs2[0].id);
+    moveTab();
+  });
 }
 // Autostart function, procesed on initial startup.
-if(tabAutostart) {
-	chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-		function(tabs){
-			//Start in main window.
-			go(tabs[0].windowId);
-		}
-	);
+if (tabAutostart) {
+  chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+    function (tabs) {
+      //Start in main window.
+      go(tabs[0].windowId);
+    }
+  );
 }
--- src/options_script.js
+++ src/options_script.js
@@ -1,141 +1,141 @@
 var bg = chrome.extension.getBackgroundPage();
 // Saves options to localStorage.
 function save_options() {
-    if (document.getElementById("autostart").checked == true) {
-        localStorage["autostart"] = 'true';
-        bg.tabInactive = true;
-    } else {
-        localStorage["autostart"] = 'false';
-        bg.tabInactive = false;
-    }
+  if (document.getElementById("autostart").checked == true) {
+    localStorage["autostart"] = 'true';
+    bg.tabInactive = true;
+  } else {
+    localStorage["autostart"] = 'false';
+    bg.tabInactive = false;
+  }
 
+  localStorage["loadurl"] = document.getElementById("loadurl").value;
+
+  if (document.getElementById("autoloadurls").checked == true) {
+    localStorage["autoloadurls"] = 'true';
     localStorage["loadurl"] = document.getElementById("loadurl").value;
-
-    if (document.getElementById("autoloadurls").checked == true) {
-        localStorage["autoloadurls"] = 'true';
-        localStorage["loadurl"] = document.getElementById("loadurl").value;
-        load_urls();
-    } else {
-        localStorage["autoloadurls"] = 'false';
-        saveUrlsAndIntervals();
-    }
+    load_urls();
+  } else {
+    localStorage["autoloadurls"] = 'false';
+    saveUrlsAndIntervals();
+  }
 
   // Update status to let user know options were saved.
   var status = document.getElementById("status");
   var status2 = document.getElementById("status2");
   status.innerHTML = "OPTIONS SAVED";
   status2.innerHTML = "OPTIONS SAVED";
-  setTimeout(function() {
+  setTimeout(function () {
     status.innerHTML = "";
     status2.innerHTML = "";
-}, 1000);
+  }, 1000);
 }
 
 // Restores saved values from localStorage.
 function restore_options() {
-    if (localStorage["autostart"]) {
-        if (localStorage["autostart"] == 'true') {
-            document.getElementById("autostart").checked = true;
-        } else {
-            document.getElementById("autostart").checked = false;
-        }
+  if (localStorage["autostart"]) {
+    if (localStorage["autostart"] == 'true') {
+      document.getElementById("autostart").checked = true;
     } else {
-        document.getElementById("autostart").checked = false;
+      document.getElementById("autostart").checked = false;
     }
-    if(localStorage["loadurl"]) {
-        document.getElementById("loadurl").value = localStorage["loadurl"];
-    }
-    if(localStorage["autoloadurls"]) {
-        if(localStorage["autoloadurls"] == 'true') {
-            document.getElementById("autoloadurls").checked = true;
-            load_urls();
-        } else {
-            document.getElementById("autoloadurls").checked = false;
-            var urlsLoad;
-            var urlsIntervalsLoad;
-            if (localStorage["urls"] && localStorage["urlsIntervals"]) {
-                urlsLoad = JSON.parse(localStorage["urls"]);
-                urlsIntervalsLoad = JSON.parse(localStorage["urlsIntervals"]);
-                var urlsString = '';
-                for(i = 0; i < urlsLoad.length; i++) {
-                    urlsString += urlsIntervalsLoad[i] + ";" + urlsLoad[i] + "\n";
-                }
-                document.getElementById("urls").value = urlsString;
-            } else {
-                document.getElementById("urls").value = "";
-                document.getElementById("urlsIntervals").value = "";
-            }
-        }
+  } else {
+    document.getElementById("autostart").checked = false;
+  }
+  if (localStorage["loadurl"]) {
+    document.getElementById("loadurl").value = localStorage["loadurl"];
+  }
+  if (localStorage["autoloadurls"]) {
+    if (localStorage["autoloadurls"] == 'true') {
+      document.getElementById("autoloadurls").checked = true;
+      load_urls();
     } else {
-        document.getElementById("autoloadurls").checked = false;
+      document.getElementById("autoloadurls").checked = false;
+      var urlsLoad;
+      var urlsIntervalsLoad;
+      if (localStorage["urls"] && localStorage["urlsIntervals"]) {
+        urlsLoad = JSON.parse(localStorage["urls"]);
+        urlsIntervalsLoad = JSON.parse(localStorage["urlsIntervals"]);
+        var urlsString = '';
+        for (i = 0; i < urlsLoad.length; i++) {
+          urlsString += urlsIntervalsLoad[i] + ";" + urlsLoad[i] + "\n";
+        }
+        document.getElementById("urls").value = urlsString;
+      } else {
+        document.getElementById("urls").value = "";
+        document.getElementById("urlsIntervals").value = "";
+      }
     }
+  } else {
+    document.getElementById("autoloadurls").checked = false;
+  }
 }
 
 // Loads the URLs and intervals from text loaded from a URL
 function load_urls() {
-    var xmlhttp = new XMLHttpRequest();
-    var url = document.getElementById("loadurl").value;
-    if(url !== "") {
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == 4) {
-                localStorage["loadurl"] = document.getElementById("loadurl").value;
-                document.getElementById("urls").value = xmlhttp.responseText;
-                saveUrlsAndIntervals();
-            }
-        }
+  var xmlhttp = new XMLHttpRequest();
+  var url = document.getElementById("loadurl").value;
+  if (url !== "") {
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState == 4) {
+        localStorage["loadurl"] = document.getElementById("loadurl").value;
+        document.getElementById("urls").value = xmlhttp.responseText;
+        saveUrlsAndIntervals();
+      }
+    };
 
-        xmlhttp.open("GET", url, true);
-        xmlhttp.send(null);
-    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send(null);
+  }
 }
 
 function saveUrlsAndIntervals() {
-    var line = document.getElementById('urls').value.split('\n');
-    var urlsArray = [];
-    var urlsIntervalsArray = [];
+  var line = document.getElementById('urls').value.split('\n');
+  var urlsArray = [];
+  var urlsIntervalsArray = [];
 
-    bg.urls = [];
-    bg.urlsIntervals = [];
-    var badLine = [];
+  bg.urls = [];
+  bg.urlsIntervals = [];
+  var badLine = [];
 
-    for(i = 0; i < line.length; i++) {
-        if(line[i] != "") {
-            if(line[i].indexOf(";") < 0) {
-                badLine.push(i);
-                console.log("Missing ;\nLine " + i + " ignored.");
-            } else {
-                var urlAndIndex = line[i].split(';');
-                if(urlAndIndex.length > 2) {
-                    badLine.push(i);
-                    console.log("Too many ';'\nLine " + i + " ignored");
-                } else if(urlAndIndex[0] == "" && urlAndIndex[1] == "") {
-                    badLine.push(i);
-                    console.log("Missing url and/or time interval.\nLine " + i + " ignored.");
-                } else if (isNaN(urlAndIndex[0])) {
-                    badLine.push(i);
-                    console.log("Time interval is not a number.\nLine " + i + " ignored.");
-                } else {
-                    urlsArray.push(urlAndIndex[1]);
-                    urlsIntervalsArray.push(urlAndIndex[0]);
+  for (i = 0; i < line.length; i++) {
+    if (line[i] != "") {
+      if (line[i].indexOf(";") < 0) {
+        badLine.push(i);
+        console.log("Missing ;\nLine " + i + " ignored.");
+      } else {
+        var urlAndIndex = line[i].split(';');
+        if (urlAndIndex.length > 2) {
+          badLine.push(i);
+          console.log("Too many ';'\nLine " + i + " ignored");
+        } else if (urlAndIndex[0] == "" && urlAndIndex[1] == "") {
+          badLine.push(i);
+          console.log("Missing url and/or time interval.\nLine " + i + " ignored.");
+        } else if (isNaN(urlAndIndex[0])) {
+          badLine.push(i);
+          console.log("Time interval is not a number.\nLine " + i + " ignored.");
+        } else {
+          urlsArray.push(urlAndIndex[1]);
+          urlsIntervalsArray.push(urlAndIndex[0]);
 
-                    bg.urls.push(urlAndIndex[1]);
-                    bg.urlsIntervals.push(urlAndIndex[0]);
-                }
-            }
+          bg.urls.push(urlAndIndex[1]);
+          bg.urlsIntervals.push(urlAndIndex[0]);
         }
+      }
     }
-    for(i = badLine.length-1; i >= 0; i--) {
-        line.splice(badLine[i], 1);
-    }
+  }
+  for (i = badLine.length - 1; i >= 0; i--) {
+    line.splice(badLine[i], 1);
+  }
 
-    var urlsAndInterals = "";
+  var urlsAndInterals = "";
 
-    for(i = 0; i < line.length; i++) {
-        urlsAndInterals = urlsAndInterals + line[i] + "\n";
-    }
-    document.getElementById('urls').value = urlsAndInterals;
-    localStorage['urls'] = JSON.stringify(urlsArray);
-    localStorage['urlsIntervals'] = JSON.stringify(urlsIntervalsArray);
+  for (i = 0; i < line.length; i++) {
+    urlsAndInterals = urlsAndInterals + line[i] + "\n";
+  }
+  document.getElementById('urls').value = urlsAndInterals;
+  localStorage['urls'] = JSON.stringify(urlsArray);
+  localStorage['urlsIntervals'] = JSON.stringify(urlsIntervalsArray);
 }
 
 // Adding listeners for restoring and saving options
